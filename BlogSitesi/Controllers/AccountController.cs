14124de06065a67de0222995415b407488edabfe
InfotechProjeler <ilica-18@hotmail.com>
2025-03-22 10:30:48 +0300

    22-03-2025 v1

--- BlogSitesi/Controllers/AccountController.cs
+++ BlogSitesi/Controllers/AccountController.cs
@@ -3,9 +3,7 @@ using BlogSitesi.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace BlogSitesi.Controllers
 {
---
+++ BlogSitesi/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using BlogSitesi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public CategoriesController(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public IActionResult Index(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest("Geçersiz İstek!");
+            }
+            var model = _databaseContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (model == null)
+            {
+                return NotFound("İlgili Post Bulunamadı!");
+            }
+            return View(model);
+        }
+    }
+}
